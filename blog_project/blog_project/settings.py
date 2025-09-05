0e363f754e6351824e893d1921d42e8d31f9331e
SmittGevariya <smitgevariya.sg141@gmail.com>
2025-09-05 13:49:29 +0530

    feat: add bootstrap and base layout

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from decouple import config
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -54,10 +54,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'blog_project.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
