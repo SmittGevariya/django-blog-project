4de1dbf6990285a1c489f851c5df02538b535de5
SmittGevariya <smitgevariya.sg141@gmail.com>
2025-09-05 13:12:21 +0530

    feat : create post detail view

--- blog_project/posts/views.py
+++ blog_project/posts/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Post
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.all()
     return render(request,'posts/post_list.html',{'posts':posts})
+
+def post_detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request,'posts/post_detail.html',{'post':post})
+
